e560628e601c1a714690656cb9eabc489bcf39a1
leonardogoandete <leonardogoandete>
2022-12-22 12:22:00 -0300

    Configurado ambientes de Prod e Hom

---
+++ env/homolog/main.tf
@@ -0,0 +1,9 @@
+module "Producao" {
+  source = "../../infra"
+
+  nome = "Homologacao"
+  descricao = "Aplicacao-de-homologacao"
+  max = 3
+  maquina = "t2.micro"
+  ambiente = "Ambiente-de-homologacao"
+}
