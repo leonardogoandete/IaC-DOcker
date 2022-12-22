ac29cd66555c155e58c4e65ad54d1042068a7c61
leonardogoandete <leonardogoandete>
2022-12-22 11:51:42 -0300

    Configurado permissoes da role

--- infra/role.tf
+++ infra/role.tf
@@ -17,3 +17,41 @@ resource "aws_iam_role" "beanstalk_ec2" {
     ]
   })
 }
+
+
+resource "aws_iam_role_policy" "beanstalk_ec2_policy" {
+  name = "beanstalk-ec2-policy"
+  role = aws_iam_role.beanstalk_ec2.id
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "cloudwatch:PutMetricData",
+          "ds:CreateComputer",
+          "ds:DescribeDirectories",
+          "ec2:DescribeInstanceStatus",
+          "logs:*",
+          "ssm:*",
+          "ec2messages:*",
+          "ecr:GetAuthorizationToken:*",
+          "ecr:BatchCheckLayerAvailability:*",
+          "ecr:GetDownloadUrlForLayer:*",
+          "ecr:GetRepositoryPolicy:*",
+          "ecr:DescribeRepositories:*",
+          "ecr:ListImages:*",
+          "ecr:DescribeImages:*",
+          "ecr:BatchGetImage:*",
+          "s3:*"
+
+          
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
