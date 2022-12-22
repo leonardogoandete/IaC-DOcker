22875675a6c5c5b881c32cdf7c1e8f8dcfc6d43b
leonardogoandete <leonardogoandete>
2022-12-22 13:02:13 -0300

    Ajustado bucket

--- infra/s3.tf
+++ infra/s3.tf
@@ -3,6 +3,9 @@ resource "aws_s3_bucket" "beanstalk_deploys" {
 }
 
 resource "aws_s3_bucket_object" "docker" {
+    depends_on = [
+      aws_s3_bucket.beanstalk_deploys
+    ]
   bucket = "${var.nome}-deploys"
   key    = "${var.nome}.zip"
   source = "${var.nome}.zip"
